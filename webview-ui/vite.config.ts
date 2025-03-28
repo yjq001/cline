d3988b31b83842266d33d330c04c0e8b7bda9c09
yangjiaqian <yangjiaqian@gf.com.cn>
2025-03-28 22:16:10 +0800

    复原图标

--- webview-ui/vite.config.ts
+++ webview-ui/vite.config.ts
@@ -5,6 +5,7 @@ import tailwindcss from "@tailwindcss/vite"
 import react from "@vitejs/plugin-react-swc"
 
 export default defineConfig({
+	base: "./",
 	plugins: [react(), tailwindcss()],
 	test: {
 		environment: "jsdom",
